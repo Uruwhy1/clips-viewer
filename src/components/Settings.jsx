3253dc57024044f2243093725fd1db99c8dbe785
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2025-01-18 21:59:27 -0300

    style settings

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,51 +1,49 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import GlobalContext from "../contexts/GlobalContext";
 import styles from "./Settings.module.css";
 import { open } from "@tauri-apps/plugin-dialog";
+import { Folder, SettingsIcon } from "lucide-react";
 
 const Settings = () => {
   const { settings, setSettings } = useContext(GlobalContext);
-  const [newGamesDir, setNewGamesDir] = useState(settings.gamesDir || "");
 
-  const handleSave = () => {
-    setSettings((prevSettings) => ({
-      ...prevSettings,
-      gamesDir: newGamesDir,
-    }));
-  };
-
-  const handleSelectFolder = async () => {
+  const handleSelectDirectory = async () => {
     const selectedDir = await open({
       directory: true,
       multiple: false,
     });
 
     if (selectedDir) {
-      setNewGamesDir(selectedDir);
+      setSettings((prevSettings) => ({
+        ...prevSettings,
+        gamesDir: selectedDir,
+      }));
     }
   };
 
   return (
     <div className={styles.settingsContainer}>
-      <h2>Settings</h2>
-      <p>Current Settings:</p>
-      <div>
-        {Object.entries(settings).map(([key, value]) => (
-          <p key={key}>
-            <strong>{key}:</strong> {value || "Not set"}
-          </p>
-        ))}
+      <div className={styles.title}>
+        <SettingsIcon size={30} />
+        <h1>Settings</h1>
       </div>
-      <div>
-        <label htmlFor="gamesDir">Games Directory:</label>
-        <input
-          type="text"
-          id="gamesDir"
-          value={newGamesDir}
-          onChange={(e) => setNewGamesDir(e.target.value)}
-        />
-        <button onClick={handleSelectFolder}>Select Folder</button>{" "}
-        <button onClick={handleSave}>Save</button>
+      <div className={`${styles.clipsDirectory} ${styles.settingItem}`}>
+        <div className={styles.title}>
+          <Folder />
+          <h3 className={styles.sectionTitle}>Clips Storage</h3>
+        </div>
+        <div className={styles.settingContainer}>
+          <div className={styles.setting}>
+            <strong>Clips Directory:</strong>
+            <p>{settings.gamesDir}</p>
+          </div>
+          <button
+            className={styles.selectFolderButton}
+            onClick={handleSelectDirectory}
+          >
+            Select Games Directory
+          </button>
+        </div>
       </div>
     </div>
   );
