bbcb43cb297b9e91634fe75c2f60f9bdb9cd341e
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2025-02-28 21:05:09 -0300

    add svg animations

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,26 @@
-import { GalleryThumbnails, LucideCirclePlay, Settings } from "lucide-react";
+import {
+  GalleryThumbnails,
+  LucideCirclePause,
+  LucideCirclePlay,
+  Settings,
+} from "lucide-react";
 import styles from "./Sidebar.module.css";
 
 const Sidebar = ({ setView, view, openSettings, settingsState }) => {
   return (
     <aside className={styles.sidebar}>
       <div
-        className={`${styles.button} ${view === "video" ? styles.active : ""}`}
+        className={`${styles.button} ${styles.video} ${
+          view === "video" ? styles.active : ""
+        }`}
         onClick={() => setView("video")}
       >
-        <LucideCirclePlay />
+        {view !== "video" ? <LucideCirclePlay /> : <LucideCirclePause />}
       </div>
       <div
-        className={`${styles.button} ${view === "clips" ? styles.active : ""}`}
+        className={`${styles.button} ${styles.clips} ${
+          view === "clips" ? styles.active : ""
+        }`}
         onClick={() => setView("clips")}
       >
         <GalleryThumbnails />
