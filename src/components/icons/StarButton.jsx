a82e5ae921a1831ffb3db030108f7fa741416b0b
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2025-03-02 20:31:48 -0300

    small fixes

--- src/components/icons/StarButton.jsx
+++ src/components/icons/StarButton.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Star } from "lucide-react";
 import styles from "./StarButton.module.css";
-const MemoizedStar = React.memo(({ ...props }) => <Star {...props} />);
 
-const StarButton = ({ active, ...restProps }) => {
+const StarButton = React.memo(({ active, ...restProps }) => {
   return (
-    <MemoizedStar
+    <Star
       {...restProps}
       className={` ${styles.favouriteButton} ${active ? styles.active : ""}`}
     />
   );
-};
+});
 
 export default StarButton;
